3b3085c39c7eaadac599bb94ba04c2c963beb58b
Marco Aurelio Schlemper <mschlemper@gmail.com>
2022-08-12 12:14:56 -0300

    commit_segundo

---
+++ banco_de_dados/bancoDeDados.php
@@ -0,0 +1,12 @@
+<?php
+$pdo = new PDO("mysql:dbname=banco_de_dados;host=localhost", "root", "");
+
+$sql = $pdo ->query('SELECT * FROM cadastro');
+
+echo "TOTAL: ".$sql->rowCount();
+
+$dados = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<pre>';
+
+print_r($dados);
