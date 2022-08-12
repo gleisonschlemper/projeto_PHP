1660f84d1ce46d715f0768316008ed6eb92e42ac
Marco Aurelio Schlemper <mschlemper@gmail.com>
2022-08-12 18:19:01 -0300

    commit_foi

--- banco_de_dados/bancoDeDados.php
+++ banco_de_dados/bancoDeDados.php
@@ -3,10 +3,10 @@ $pdo = new PDO("mysql:dbname=banco_de_dados;host=localhost", "root", "");
 
 $sql = $pdo ->query('SELECT * FROM cadastro');
 
-echo "TOTAL: ".$sql->rowCount();
+//echo "TOTAL: ".$sql->rowCount();
 
 $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<pre>';
+//echo '<pre>';
 
-print_r($dados);
+//print_r($dados);
---
+++ tabelaDeDados.php
@@ -0,0 +1,35 @@
+<?php
+
+require '../projeto_PHP/banco_de_dados/bancoDeDados.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+    <title>Tabela de cadastro</title>
+</head>
+<body>
+    <header>
+        <a href="">fazer cadastro</a>
+    </header>
+   <table  border="1" width="100%">
+        <tr>
+            <th>id</th>
+            <th>nomes</th>
+            <th>email</th>
+            <th>senha</th>
+        </tr>
+   </table> 
+</body>
+</html>
